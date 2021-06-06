e5740e9c2e7c5273f00c19be0c2797301cde151c
Viktar Pakhomau <victor@ktc.rw>
2021-06-06 22:39:49 +0300

    Preparation for locales

---
+++ locale.h
@@ -0,0 +1,65 @@
+/* 
+ * File:   locale.h
+ * Author: victor
+ *
+ * Created on 6 июня 2021 г., 22:31
+ */
+
+#ifndef LOCALE_H
+#define	LOCALE_H
+
+#define HOUR_SYMBOL 'h'
+#define LITRE_SYMBOL 'l'
+#define VOLT_SYMBOL 'V'
+#define KM1_SYMBOL 'k'
+#define KM2_SYMBOL 'm'
+#define SECONDS_SYMBOL 's'
+#define CELSIUS_SYMBOL 0xDF
+
+// custom lcd characters values
+#define _kmh0   0x00
+#define _kmh1	0x01
+#define _omin0	0x02
+#define _omin1	0x03
+#define _lkm0	0x04
+#define _lkm1	0x05
+#define _lh0	0x06
+#define _lh1    0x07
+
+const char empty_string[] = "----";
+const char no_time_string[] = "-----'--";
+const char trip_string[] = "trip ";
+const char onoff_string[] = "\0 OFF\0  ON";
+const char time_correction[] = "time correction?";
+const char reset_string[] = "reset?"; 
+const char speed100_string[] = "0-100 timing"; 
+const char speed100_wait_string[] = "wait for start"; 
+const char timeout_string[] = "timeout"; 
+
+const char service_menu_title[] = "SERVICE MENU";
+const char service_counters[] = "\0engine hours\0engine oil\0gearbox oil\0air filter\0spark plugs";
+const char settings_bits[] = "\0dual inj\0skip temp\0key sound\0t'engn alarm\0serv alarm\0\0\0";
+const char temp_sensors[] = "\0---\0out\0in\0eng";
+
+const char fuel_constant_str[] = "FUEL constant";
+const char vss_constant_str[] = "VSS constant";
+const char total_trip_str[] = "total trip";
+const char voltage_adjust_str[] = "voltage adjust";
+const char settings_bits_str[] = "settings bits";
+const char temp_sensor_str[] = "temp sensors";
+const char service_counters_str[] = "service cntrs";
+
+const char day_of_week_str[] = "\0sunday\0monday\0tuesday\0wednesday\0thursday\0friday\0saturday";
+const char month_str[] = "\0jan\0feb\0mar\0apr\0may\0jun\0jul\0aug\0sep\0oct\0nov\0dec";
+
+#define DATA_KMH_0  0x05, 0x06, 0x05, 0x00, 0x00, 0x00, 0x01, 0x02
+#define DATA_KMH_1  0x0A, 0x15, 0x15, 0x00, 0x10, 0x05, 0x07, 0x01
+#define DATA_OMIN_0 0x07, 0x05, 0x07, 0x00, 0x00, 0x01, 0x02, 0x00
+#define DATA_OMIN_1 0x00, 0x00, 0x08, 0x10, 0x00, 0x0A, 0x15, 0x15
+#define DATA_L100_0 0x0C, 0x14, 0x14, 0x01, 0x02, 0x05, 0x01, 0x01
+#define DATA_L100_1 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x15, 0x1F
+#define DATA_LH_0   0x03, 0x05, 0x05, 0x00, 0x00, 0x01, 0x02, 0x00
+#define DATA_LH_1   0x00, 0x00, 0x08, 0x10, 0x00, 0x14, 0x1C, 0x04
+
+#endif	/* LOCALE_H */
+
--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <xc.h>
 
 #include "hw.h"
+#include "locale.h"
 #include <stdbool.h>
 #include <string.h>
 #include "lcd.h"
@@ -22,7 +23,7 @@
 #define USE_DS18B20
 
 // place custom chars data in eeprom
-#define EEPROM_CUSTOM_CHARS
+//#define EEPROM_CUSTOM_CHARS
 
 // * timer1 resolution * 0,01ms
 #define MAIN_INTERVAL 200
@@ -222,68 +223,24 @@ ds_time time;
 
 unsigned char fuel1_const;
 
-#define HOUR_SYMBOL 'h'
-#define LITRE_SYMBOL 'l'
-#define VOLT_SYMBOL 'V'
-#define KM1_SYMBOL 'k'
-#define KM2_SYMBOL 'm'
-#define SECONDS_SYMBOL 's'
-#define CELSIUS_SYMBOL 0xDF
-
-// custom lcd characters values
-#define _kmh0   0x00
-#define _kmh1	0x01
-#define _omin0	0x02
-#define _omin1	0x03
-#define _lkm0	0x04
-#define _lkm1	0x05
-#define _lh0	0x06
-#define _lh1    0x07
-
-const char empty_string[] = "----";
-const char no_time_string[] = "-----'--";
-const char trip_string[] = "trip ";
-const char onoff_string[] = "\0 OFF\0  ON";
-const char time_correction[] = "time correction?";
-const char reset_string[] = "reset?"; 
-const char speed100_string[] = "0-100 timing"; 
-const char speed100_wait_string[] = "wait for start"; 
-const char timeout_string[] = "timeout"; 
-
-const char service_menu_title[] = "SERVICE MENU";
-const char service_counters[] = "\0engine hours\0engine oil\0gearbox oil\0air filter\0spark plugs";
-const char settings_bits[] = "\0dual inj\0skip temp\0key sound\0t'engn alarm\0serv alarm\0\0\0";
-const char temp_sensors[] = "\0---\0out\0in\0eng";
-
-const char fuel_constant_str[] = "FUEL constant";
-const char vss_constant_str[] = "VSS constant";
-const char total_trip_str[] = "total trip";
-const char voltage_adjust_str[] = "voltage adjust";
-const char settings_bits_str[] = "settings bits";
-const char temp_sensor_str[] = "temp sensors";
-const char service_counters_str[] = "service cntrs";
-
-const char day_of_week_str[] = "\0sunday\0monday\0tuesday\0wednesday\0thursday\0friday\0saturday";
-const char month_str[] = "\0jan\0feb\0mar\0apr\0may\0jun\0jul\0aug\0sep\0oct\0nov\0dec";
-
 #ifdef EEPROM_CUSTOM_CHARS
-#define CUSTOM_CHAR_DATA(a0,a1,a2,a3,a4,a5,a6,a7) __EEPROM_DATA(a0,a1,a2,a3,a4,a5,a6,a7);
+#define CUSTOM_CHAR_DATA(a) __EEPROM_DATA(a);
 #else
-#define CUSTOM_CHAR_DATA(a0,a1,a2,a3,a4,a5,a6,a7) a0,a1,a2,a3,a4,a5,a6,a7,
+#define CUSTOM_CHAR_DATA(a) a,
 #endif
 
 #ifndef EEPROM_CUSTOM_CHARS
 // custom lcd characters data
 const unsigned char custom_chars[] = {
 #endif    
-CUSTOM_CHAR_DATA(0x05, 0x06, 0x05, 0x00, 0x00, 0x00, 0x01, 0x02) // kmh[0]
-CUSTOM_CHAR_DATA(0x0A, 0x15, 0x15, 0x00, 0x10, 0x05, 0x07, 0x01) // kmh[1]
-CUSTOM_CHAR_DATA(0x07, 0x05, 0x07, 0x00, 0x00, 0x01, 0x02, 0x00) // omin[0]
-CUSTOM_CHAR_DATA(0x00, 0x00, 0x08, 0x10, 0x00, 0x0A, 0x15, 0x15) // omin[1]
-CUSTOM_CHAR_DATA(0x0C, 0x14, 0x14, 0x01, 0x02, 0x05, 0x01, 0x01) // L100[0]
-CUSTOM_CHAR_DATA(0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x15, 0x1F) // L100[1]
-CUSTOM_CHAR_DATA(0x03, 0x05, 0x05, 0x00, 0x00, 0x01, 0x02, 0x00) // l/h[0]
-CUSTOM_CHAR_DATA(0x00, 0x00, 0x08, 0x10, 0x00, 0x14, 0x1C, 0x04) // l/h[1]
+CUSTOM_CHAR_DATA(DATA_KMH_0)    // kmh[0]
+CUSTOM_CHAR_DATA(DATA_KMH_1)    // kmh[1]
+CUSTOM_CHAR_DATA(DATA_OMIN_0)   // omin[0]
+CUSTOM_CHAR_DATA(DATA_OMIN_1)   // omin[1]
+CUSTOM_CHAR_DATA(DATA_L100_0)   // L100[0]
+CUSTOM_CHAR_DATA(DATA_L100_1)   // L100[1]
+CUSTOM_CHAR_DATA(DATA_LH_0)     // l/h[0]
+CUSTOM_CHAR_DATA(DATA_LH_1)     // l/h[1]
 #ifndef EEPROM_CUSTOM_CHARS
 };
 #endif
