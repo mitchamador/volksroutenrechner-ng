17d496a09ee6de5304e566ddc5b8c4c6ad5f8d9c
Viktar Pakhomau <victor@ktc.rw>
2021-08-20 15:22:37 +0300

    Update configuration

--- version.cmd
+++ version.cmd
@@ -1,3 +1,27 @@
+:; if [ -z 0 ]; then
+  @echo off
+  goto :WINDOWS
+fi
+
+echo \#ifndef VERSION_H >include/version.h
+echo \#define VERSION_H >>include/version.h
+echo >>include/version.h
+echo \#define VERSION_STRING \"$(date +"%H:%M %d.%m.%Y")\"\; >>include/version.h
+echo >>include/version.h
+echo \#define VERSION_MINUTE_BCD 0x$(date +%M) >>include/version.h
+echo \#define VERSION_HOUR_BCD 0x$(date +%H) >>include/version.h
+echo \#define VERSION_DAY_OF_WEEK_BCD 0x0$(date +%w) >>include/version.h
+echo \#define VERSION_DAY_OF_MONTH_BCD 0x$(date +%d) >>include/version.h
+echo \#define VERSION_MONTH_BCD 0x$(date +%m) >>include/version.h
+echo \#define VERSION_YEAR_BCD 0x$(date +%y) >>include/version.h
+echo >>include/version.h
+echo \#endif \/\* VERSION_H \*\/ >>include/version.h
+
+# bash stuff
+exit
+
+:WINDOWS
+
 @echo off
 
 for /f "delims=" %%a in ('wmic path win32_localtime get dayofweek /format:list ') do for /f "delims=" %%d in ("%%a") do set %%d
@@ -17,4 +41,4 @@ echo #define VERSION_YEAR_BCD 0x%date:~8,2% >>include\version.h
 echo. >>include\version.h
 echo #endif	/* VERSION_H */ >>include\version.h
 
-
+:: windows stuff
