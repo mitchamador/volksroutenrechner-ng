7eca69d55763da594339841c7254d58d81078849
mitchamador <vicokenator@gmail.com>
2022-07-16 20:20:26 +0300

    WIP: Fix i2c eeprom reading. Update eeprom.

--- src/i2c-eeprom.c
+++ src/i2c-eeprom.c
@@ -17,7 +17,7 @@ void I2C_read_eeprom_block(unsigned char* p, uint16_t ee_addr, unsigned char len
     if (ack == ACK) {
         I2C_Master_Write(ee_addr & 0xFF);
         I2C_Master_RepeatedStart();
-        I2C_Master_Write(0xA1);
+        I2C_Master_Write(0xA1 | ((ee_addr >> 7) & 0x0E));
         while (--length > 0) {
             *p++ = I2C_Read_Byte(ACK);
         }
